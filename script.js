f0c747b6c949c2ed847cfb47df661eedcc835e02
Szakál Kristóf Gallért <dyy0e5@inf.elte.hu>
2021-12-04 18:26:57 +0100

    big changes

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-    if(navbarLinks.classList[1] == 'active'){
-        disableScrolling()
-        console.log("scrolling disabled")
-    }else{
-        enableScrolling()
-        console.log("scrolling enabled")
-    }
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  if (navbarLinks.classList[1] == "active") {
+    disableScrolling();
+    console.log("scrolling disabled");
+  } else {
+    enableScrolling();
+    console.log("scrolling enabled");
+  }
 });
 
-function disableScrolling(){
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){window.scrollTo(x, y);};
+
+function disableScrolling() {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
 }
 
-function enableScrolling(){
-    window.onscroll=function(){};
+function enableScrolling() {
+  window.onscroll = function () {};
 }
 
 
