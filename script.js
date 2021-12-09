d284c755ed878cac56ac59adb77e09d39856eece
Szakál Kristóf Gallért <dyy0e5@inf.elte.hu>
2021-12-09 23:17:37 +0100

    scrolling

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
-  
+
   if (navbarLinks.classList[1] == "active") {
     document.body.style.overflow = 'hidden';
+    $('body').bind('touchmove', function(e){e.preventDefault()})
     disableScrolling();
     console.log("scrolling disabled");
   } else {
-    document.body.style.overflow = 'scroll';
+    document.body.style.overflow = 'auto';
+    $('body').unbind('touchmove')
     enableScrolling();
     console.log("scrolling enabled");
   }
-  
   
 });
 
